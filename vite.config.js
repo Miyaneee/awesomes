a08d70086a9f3a4ca5cd3279ab50247f29bfe1a4
Gu-Miao <1450206741@qq.com>
2021-11-18 18:03:35 +0800

    Add plugin to load json5

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import json5 from '@miyaneee/rollup-plugin-json5'
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [json5(), solidPlugin()],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
