a782106b4489d00a6e7d54fa84de1a00d8bc1869
Gu-Miao <1450206741@qq.com>
2021-11-18 18:05:45 +0800

    Add some simple data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { createSignal } from 'solid-js'
+
+import data from './data.json5'
+
 function App() {
-  return <div></div>
+  const [value, setValue] = createSignal('')
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={value()}
+        onInput={e => setValue(e.target.value)}
+        autoComplete={false}
+      />
+      <ul>
+        {data
+          .filter(item => item.name.includes(value()))
+          .map(item => (
+            <li key="item.name">{item.name}</li>
+          ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App
